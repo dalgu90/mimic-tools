4c9bcf1808894d6445a7055c38d5f57b3db1a7c4
Julien Tourille <julien@latribu.org>
2017-08-22 11:20:27 +0200

    Switching to non web based version of ctakes

--- mimic_w2v_tools/ctakes.py
+++ mimic_w2v_tools/ctakes.py
@@ -3,28 +3,33 @@ import json
 import logging
 import os
 import re
+import shutil
 import subprocess
 from collections import defaultdict
 
-import progressbar
-import requests
 from joblib import Parallel, delayed
+from lxml import etree
 
-from .tools import remove_abs, ensure_dir, get_other_extension
+from .tools import remove_abs, ensure_dir
 
 
-def ctakes_corpus(corpus_path, working_dir, ctakes_server_location):
+def ctakes_corpus(corpus_path, working_dir, ctakes_dir, java_dir, resources_dir):
     """
     Process a clean mimic-iii corpus with ctakes (via remote server)
     :param corpus_path: path to the clean mimic-iii-corpus
     :param working_dir: path where gzipped json files will be created
-    :param ctakes_server_location: cTAKES server network address
+    :param ctakes_dir: path to ctakes installation directory
+    :param java_dir: path to java binaries directory
+    :param resources_dir: path to resource directory where cTAKES templates are stored
     :return: nothing
     """
 
     # Target document path creation
     document_output_path = os.path.join(os.path.abspath(working_dir), "documents")
+    log_output_path = os.path.join(os.path.abspath(working_dir), "logs")
+
     ensure_dir(document_output_path)
+    ensure_dir(log_output_path)
 
     logging.info("Counting documents")
 
@@ -32,67 +37,76 @@ def ctakes_corpus(corpus_path, working_dir, ctakes_server_location):
 
     for root, dirs, files in os.walk(os.path.abspath(corpus_path)):
         for _ in files:
+            subdir = remove_abs(re.sub(os.path.abspath(corpus_path), "", root))
+            target_dir = os.path.join(os.path.abspath(document_output_path), subdir)
+            ensure_dir(target_dir)
             nb_files += 1
 
-    pbar = progressbar.ProgressBar(
-        max_value=nb_files,
-        widgets=[
-            progressbar.Percentage(),
-            ' (', progressbar.SimpleProgress(), ') ',
-            progressbar.Bar(),
-            ' ', progressbar.Timer(), ' ',
-            progressbar.AdaptiveETA()
-        ]
-    )
+    logging.info("* Number of files to process: {}".format(nb_files))
 
-    logging.info("Start processing")
+    source_analysis_engine = os.path.join(os.path.abspath(resources_dir), "AggregatePlaintextFastUMLSProcessor.xml")
+    target_analysis_engine = os.path.join(os.path.abspath(ctakes_dir),
+                                          "desc/ctakes-clinical-pipeline/desc/analysis_engine/"
+                                          "custom_AggregatePlaintextFastUMLSProcessor.xml")
 
-    # Going trough the files
-    track_nb = 0
-    skipped = 0
+    shutil.copy(source_analysis_engine, target_analysis_engine)
 
-    with pbar as bar:
-        for root, dirs, files in os.walk(os.path.abspath(corpus_path)):
-            for filename in files:
+    for dirname in os.listdir(os.path.abspath(corpus_path)):
+        if os.path.isdir(os.path.join(os.path.abspath(corpus_path), dirname)):
 
-                # Creating target subdirectory according to source subdirectory
-                subdir = remove_abs(re.sub(os.path.abspath(corpus_path), "", root))
-                target_dir = os.path.join(os.path.abspath(document_output_path), subdir)
+            source_dir = os.path.join(os.path.abspath(corpus_path), dirname)
+            target_dir = os.path.join(os.path.abspath(document_output_path), dirname)
 
-                ensure_dir(target_dir)
+            ctakes_err_log_filename = os.path.join(log_output_path, "{}-err.log".format(dirname))
+            ctakes_out_log_filename = os.path.join(log_output_path, "{}-out.log".format(dirname))
 
-                # Source and target file path creation
-                source_file = os.path.join(root, filename)
-                target_file = os.path.join(os.path.abspath(target_dir), get_other_extension(filename, "json"))
+            logging.info("* Processing {}".format(dirname))
 
-                # Reading source file content
-                txt_content = open(source_file, "r", encoding="UTF-8").read()
+            source_xml_file = os.path.join(os.path.abspath(ctakes_dir),
+                                           "desc/ctakes-clinical-pipeline/desc/collection_processing_engine/"
+                                           "test_plaintext.xml")
+            target_xml_file = os.path.join(os.path.abspath(ctakes_dir),
+                                           "desc/ctakes-clinical-pipeline/desc/collection_processing_engine/"
+                                           "test_plaintext_copy.xml")
 
-                if len(txt_content) == 0:
+            namespaces = {
+                'uima': 'http://uima.apache.org/resourceSpecifier'
+            }
+            tree = etree.parse(source_xml_file)
+            tree.find('.//uima:nameValuePair[uima:name="InputDirectory"]/uima:value/uima:string',
+                      namespaces).text = source_dir
+            tree.find('.//uima:nameValuePair[uima:name="outputDir"]/uima:value/uima:string',
+                      namespaces).text = target_dir
+            tree.find(".//uima:casProcessor[@name='AggregatePlaintextProcessor']/uima:descriptor/uima:import",
+                      namespaces).set("location", "../analysis_engine/custom_AggregatePlaintextFastUMLSProcessor.xml")
 
-                    track_nb += 1
-                    skipped += 1
-                    bar.update(track_nb)
-                    continue
+            tree.write(target_xml_file, pretty_print=True, xml_declaration=True, encoding='UTF-8')
 
-                # Sending file to cTAKES server
-                r = requests.post(ctakes_server_location, data=txt_content.encode("UTF-8"), timeout=None)
+            logging.info(" ".join(["{}/java".format(java_dir), '-Dctakes.umlsuser=julien',
+                                   '-Dctakes.umlspw=sredCk7wqJAPz74k1oDR', '-cp',
+                                   '{}/desc/:{}/resources/:{}/lib/*'.format(ctakes_dir, ctakes_dir, ctakes_dir),
+                                   '-Dlog4j.configuration=file:{}/config/log4j.xml'.format(ctakes_dir),
+                                   '-Xms1024M', '-Xmx3g', 'org.apache.ctakes.core.cpe.CmdLineCpeRunner',
+                                   target_xml_file]))
 
-                logging.debug("* Status: {}".format(r.status_code))
+            subprocess.call(["{}/java".format(java_dir), '-Dctakes.umlsuser=julien',
+                             '-Dctakes.umlspw=sredCk7wqJAPz74k1oDR', '-cp',
+                             '{}/desc/:{}/resources/:{}/lib/*'.format(ctakes_dir, ctakes_dir, ctakes_dir),
+                             '-Dlog4j.configuration=file:{}/config/log4j.xml'.format(ctakes_dir),
+                             '-Xms1024M', '-Xmx3g', 'org.apache.ctakes.ytex.tools.RunCPE',
+                             target_xml_file],
+                            cwd=ctakes_dir,
+                            stdout=open(ctakes_out_log_filename, "w", encoding="UTF-8"),
+                            stderr=open(ctakes_err_log_filename, "w", encoding="UTF-8"))
 
-                # Reading response json content
-                r_content = r.json()
+            os.remove(target_xml_file)
 
-                # Dumping json response to disk (target file location)
-                json.dump(r_content, open(target_file, "w", encoding="UTF-8"))
+            for root, dirs, files in os.walk(target_dir):
+                for filename in files:
+                    if re.match("^.*\.xml$", filename):
+                        subprocess.call(['gzip', '--best', filename], cwd=root)
 
-                # Gzipping file
-                subprocess.call(['gzip', '--best', os.path.basename(target_file)], cwd=target_dir)
-
-                track_nb += 1
-                bar.update(track_nb)
-                
-                logging.info("* Processed {} | Skipped {} | Total {}".format(track_nb, skipped, nb_files))
+    os.remove(target_analysis_engine)
 
 
 def ctakes_to_txt(json_path, txt_path, output_path, n_jobs=1):
@@ -130,7 +144,7 @@ def _convert_ctakes_file(root, filename, subdir, document_output_path, txt_path)
     target_txt_filename = os.path.join(target_path, "{}.txt".format(filename.split(".")[0]))
 
     json_content = json.load(gzip.open(source_json_filename, 'rt'))
-    txt_content = open(source_txt_filename, "r", encoding="UTF-8").read()
+    txt_content = open(source_txt_filename, "r", encoding="UTF-8", newline='').read().encode("UTF-8")
     sentences = _fetch_sentences(json_content)
 
     with open(target_txt_filename, "w", encoding="UTF-8") as output_file:
@@ -156,7 +170,7 @@ def _fetch_sentences(json_content):
 
     for ann in json_content:
         if ann["typ"] == sentence_type:
-            sentences[(ann["annotation"]["begin"],ann["annotation"]["end"])] = list()
+            sentences[(ann["annotation"]["begin"], ann["annotation"]["end"])] = list()
 
     for ann in json_content:
         if ann["typ"] in entity_types:
